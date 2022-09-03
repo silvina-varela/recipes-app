f36fe7bb9bfab75f6eee064b96305e6be1205267
silvina-varela <varelasilvina@gmail.com>
2022-09-02 21:01:03 -0300

    deployed

--- client/src/components/Error404/Error404.jsx
+++ client/src/components/Error404/Error404.jsx
@@ -1,9 +1,15 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
+
+import "./error404.css";
+
 
 const Error404 = () => {
     return(
-        <div>
-            <h1 className='home-body not-found'>Page Not Found :/</h1>
+        <div className='home-body error404'>
+            <h1 className='not-found'>Page Not Found :/</h1>
+            <Link to="/home"><button className='error-btn'>Go back</button></Link>
+            
         </div>
     )
 }
